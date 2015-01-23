4c0834e3d61211ad1c83a6650c457b8ce42784cc
user <user@localhost.localdomain>
2015-01-23 11:41:53 +0900

    테스트

---
+++ test.py
@@ -0,0 +1,15 @@
+__author__ = 'user'
+"""Unit test cases for hangman game."""
+
+import unittest
+import hangman
+class HangmanTestCase(unittest.TestCase):
+    def test_checkCorrectAnswer(self):
+        answer = hangman.checkCorrectAnswer("baon", "baboon")
+        self.assertTrue(answer)
+    def test_checkWrongAnswer(self):
+        answer = hangman.checkWrongAnswer("zebrio", "zebra")
+        self.assertTrue(answer)
+
+if __name__ == "__main__":
+    unittest.main()
